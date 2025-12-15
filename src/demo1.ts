89b5e3f83e3ac90c19014c88e107ccc19b51c2e7
buiduchuy <huyxhhz@gmail.com>
2025-12-15 23:01:55 +0700

    finish task

--- src/demo1.ts
+++ src/demo1.ts
@@ -1,9 +1,53 @@
-type Name = {
-    name:string,
-    age: number
+// create a new type to control the property of the student and the Course
+// Tstudent = Type Student, Tcourse = Type Course
+type Tstudent = {
+    id: number,
+    name: string,
+    email?: string,
+    isPremium: boolean,
+    contact?: string|number
 }
-let a: Name = {
-    name: 'huy',
-    age: 14
+type Tcourse ={
+    courseId: number,
+    title: string,
+    price: number,
+    students: Tstudent[]
 }
-console.log(a);
+const student1: Tstudent = {
+    id: 1,
+    name: "Bui Duc Huy",
+    isPremium: true
+}
+const student2: Tstudent = {
+    id: 2,
+    name: "Nguyen Van An",
+    isPremium: false
+}
+const English:Tcourse = {
+    courseId: 1,
+    title:"Học tiếng anh",
+    price:5000,
+    students: []
+}
+const registerStudentToCourse = (student: Tstudent, course:Tcourse) :boolean=>{
+    const isExist = course.students.some(std =>std.id === student.id)
+    if(isExist){
+        return false
+    }
+    course.students.push(student)
+    return true
+
+}
+const printCourseInfo = (course: Tcourse):void =>{
+    if(course.students.length == 0){
+        console.log("Danh sách trống");
+        return
+    }
+    const studentList = course.students.map(std =>std.name)
+    console.log(studentList.join(","));
+    
+}
+registerStudentToCourse(student1,English)
+registerStudentToCourse(student2,English)
+printCourseInfo(English)
+printCourseInfo(English)
